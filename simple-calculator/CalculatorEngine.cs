7f4c2686f392b25fb4344de75e719d8d654636ef
Gvxng <yanisachamou@gmail.com>
2025-09-14 23:59:08 -0400

    Update CalculatorEngine.cs

--- simple-calculator/CalculatorEngine.cs
+++ simple-calculator/CalculatorEngine.cs
@@ -4,37 +4,53 @@ namespace SimpleCalculator
 {
     public static class CalculatorEngine
     {
-        public static double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        public static string Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
         {
             //You can use switch case also here for multiple operations or use if-else-if ladder
             double result = 0;
+            string text = "";
 
             if (argOperation == "+" || argOperation.ToLower() == "add")
             {
                 result = argFirstNumber + argSecondNumber;
+                text = "plus";
             }
-            else if (argOperation == "-" || argOperatuin.ToLower() == "substract")
+            else if (argOperation == "-" || argOperation.ToLower() == "substract")
             {
                 result = argFirstNumber - argSecondNumber;
+                text = "minus";
             }
             else if (argOperation == "*" || argOperation.ToLower() == "multiply")
             {
                 result = argFirstNumber * argSecondNumber;
+                text = "multiplied by";
             }
             else if (argOperation == "/" || argOperation.ToLower() == "divide")
             {
                 result = argFirstNumber / argSecondNumber;
+                text = "divided by";
                 }
             else 
             {
-            console.writeLine("You have entered an invalid operation. Valid operation are:");
-            console.writeLine("Enter  + or add");
-            console.writeLine("Enter  - or substract");
-            console.writeLine("Enter  * or multiply");
-            console.writeLine("Enetr  / or divide");
+            console.WriteLine("You have entered an invalid operation. Valid operation are:");
+            console.WriteLine("Enter  + or add");
+            console.WriteLine("Enter  - or subtract");
+            console.WriteLine("Enter  * or multiply");
+            console.WriteLine("Enter  / or divide");
+                return "";
             }
-
-            return result;
+            
+           StringBuilder sb = new StringBuilder();
+            sb.Append("The value ");
+            sb.Append(argFirstNumber.ToString("F2"));
+            sb.Append(" ");
+            sb.Append(text);
+            sb.Append(" the value ");
+            sb.Append(argSecondNumber.ToString("F2"));
+            sb.Append(" is equal to ");
+            sb.Append(result.ToString("F2"));
+            sb.Append(".");
+            return sb.ToString();
         }
     }
 }
