b4cad640739e2212891bc0888dfa7735558588d8
Gvxng <yanisachamou@gmail.com>
2025-09-14 21:59:19 -0400

    Update CalculatorEngine.cs

--- simple-calculator/CalculatorEngine.cs
+++ simple-calculator/CalculatorEngine.cs
@@ -2,9 +2,9 @@
 
 namespace SimpleCalculator
 {
-    public class CalculatorEngine
+    public static class CalculatorEngine
     {
-        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        public static double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
         {
             //You can use switch case also here for multiple operations or use if-else-if ladder
             double result = 0;
